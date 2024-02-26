f1181c36a6b75a9ff43264b3abfb1e75ec167b5b
jamik-dev <jamik.oik.group@gmail.com>
2024-02-26 21:45:05 +0500

    setup project

---
+++ src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>HOME PAGE</h1>
+</template>
+
+<script setup>
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
